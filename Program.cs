8a5f1e91ad548d7a2d7b7432dc98379f4c9634b7
Sumit Saha <sahasumit288@gmail.com>
2019-04-12 03:58:20 +0600

    reading from file and writing at console

--- Program.cs
+++ Program.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System;
 
-namespace C_
+namespace TeleprompterConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+
+
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var pause = Task.Delay(200);
+                    // Synchronously waiting on a task is an
+                    // anti-pattern. This will get fixed in later
+                    // steps.
+                    pause.Wait();
+                }
+            }
+        }
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            var lineLength = 0;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var words = line.Split(' ');
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+                        lineLength += word.Length + 1;
+                        if (lineLength > 70)
+                        {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
+                    //yield return Environment.NewLine;
+                }
+            }
         }
     }
+
 }
